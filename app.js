345d803c08c809e9ee52968276e0ed74f5339276
Yerkebulan Yessenali <yerkebulanyessenali03@gmail.com>
2023-09-14 15:30:03 +0600

    parse post requests to the server using body parser

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const https = require("https");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.get("/", function(req, res){
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=Almaty&appid=4db0bcb363208ddf403a9da11323059e&units=metric";
+    res.sendFile(__dirname+"/index.html");
+});
+
+app.post("/", function(req, res){
+    const query = req.body.cityName;
+    const apiKey = "4db0bcb363208ddf403a9da11323059e";
+    const unit = "metric";
+    const url = "https://api.openweathermap.org/data/2.5/weather?q="+query+"&appid="+apiKey+"&units="+unit;
     https.get(url, function(response){
         console.log(response);
         response.on("data", function(data){
@@ -14,12 +24,14 @@ app.get("/", function(req, res){
             const icon = weatherData.weather[0].icon;
             const imageURL = "https://openweathermap.org/img/wn/"+icon+"@2x.png";
             res.write("<p>The weather is currently "+weatherDescription+"</p>");
-            res.write("<h1>The temperature in Almaty is "+temp+" degrees Celcius.</h1>");
+            res.write("<h1>The temperature in "+query+" is "+temp+" degrees Celcius.</h1>");
             res.write("<img src="+imageURL+">");
             res.send();
         })
     });
 });
+
+
 
 app.listen(3000, function(){
     console.log("Server is running on port 3000");
