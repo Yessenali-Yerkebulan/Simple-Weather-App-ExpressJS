79bba7f5a82f89667a89b9cb7079f88f18046f64
Yerkebulan Yessenali <yerkebulanyessenali03@gmail.com>
2023-09-14 09:59:00 +0600

    render a website with live api data

--- app.js
+++ app.js
@@ -11,11 +11,14 @@ app.get("/", function(req, res){
             const weatherData = JSON.parse(data);
             const temp = weatherData.main.feels_like;
             const weatherDescription = weatherData.weather[0].description;
-            console.log(weatherDescription);
-            console.log(temp);
+            const icon = weatherData.weather[0].icon;
+            const imageURL = "https://openweathermap.org/img/wn/"+icon+"@2x.png";
+            res.write("<p>The weather is currently "+weatherDescription+"</p>");
+            res.write("<h1>The temperature in Almaty is "+temp+" degrees Celcius.</h1>");
+            res.write("<img src="+imageURL+">");
+            res.send();
         })
     });
-    res.send("Server is up and running");
 });
 
 app.listen(3000, function(){
