61df40b8539a36bf05f714fdd242e8be4ece970f
Yerkebulan Yessenali <yerkebulanyessenali03@gmail.com>
2023-09-14 09:32:15 +0600

    get json data from openweather

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const https = require("https");
+
+const app = express();
+
+app.get("/", function(req, res){
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=Almaty&appid=4db0bcb363208ddf403a9da11323059e";
+    https.get(url, function(response){
+        console.log(response);
+    });
+    res.send("Server is up and running");
+});
+
+app.listen(3000, function(){
+    console.log("Server is running on port 3000");
+});
